07a5a0d880b8819b6492475c005ce7e9153816a1
ELİF DENK <elifdenkk@gmail.com>
2023-07-04 01:21:09 +0300

    generics

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -15,6 +15,20 @@ public class Main {
 
         printDoubled(items);
 
+        System.out.println("---------------------------------------------");
+
+        FootballPlayer joe = new FootballPlayer("joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<SoccerPlayer> liverpool = new Team("Liverpool");
+
+        // liverpool.addPlayer(joe);
+       // liverpool.addPlayer(pat);
+        liverpool.addPlayer(beckham);
+
+
+
 
     }
 
