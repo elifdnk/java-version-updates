f10500d73e55a6bd04a2f92efb653f8503194eac
ELİF DENK <elifdenkk@gmail.com>
2023-07-07 16:34:10 +0300

    Generics

---
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -0,0 +1,17 @@
+package com.cydeo;
+
+public class ArraySorting { //QuickSort, BubbleSort
+    public static void main(String[] args) {
+
+        ArraySorting as = new ArraySorting();
+        as.sort();
+
+
+    }
+
+    private void sort() {
+        System.out.println("Sorting array");
+    }
+
+
+}
