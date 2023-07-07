6cbc1ff1db572faaead4b451ed0c0af648dcdab3
ELİF DENK <elifdenkk@gmail.com>
2023-07-07 15:47:18 +0300

    Generics

--- m1-generics/src/main/java/com/cydeo/task/Algorithm.java
+++ m1-generics/src/main/java/com/cydeo/task/Algorithm.java
@@ -4,7 +4,7 @@ public final class Algorithm {
 
   //  public static <T> T max(T x, T y) {
 
-  //   return x > y ? x : y;    // x>y  is wrong. because we do not know x and integer or no. just integer use > .
+  //   return x > y ? x : y;    // x>y  is wrong. because generics use Non_primitive data types and non-primitive data type is object. we cant say one object grater than other object
   //  }
 
     }
