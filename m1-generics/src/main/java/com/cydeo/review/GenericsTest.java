bd82beb9acf34ecfb8545c003894bb323f1345b7
ELİF DENK <elifdenkk@gmail.com>
2023-07-09 19:35:04 +0300

    Generic Review

--- m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
+++ m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
@@ -7,14 +7,14 @@ public class GenericsTest {
     public static void main(String[] args) {
 
         List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student("Mike",101));
-        studentList.add(new Student("Joe",102));
-        studentList.add(new Student("Adam",103));
+        studentList.add(new Student("Mike", 101));
+        studentList.add(new Student("Joe", 102));
+        studentList.add(new Student("Adam", 103));
 
 
         List<Teacher> teacherList = new ArrayList<>();
-        teacherList.add(new Teacher("Albus",201));
-        teacherList.add(new Teacher("Severus",202));
+        teacherList.add(new Teacher("Albus", 201));
+        teacherList.add(new Teacher("Severus", 202));
 
         printInfo(studentList);
         printInfo(teacherList);
@@ -23,19 +23,24 @@ public class GenericsTest {
     }
     //write a method that takes a list of students, prints each student and the total students count
 
-    public static <T> void printInfo (List<T> personList ){
-        for (T person : personList ){
+    public static <T> void printInfo(List<T> personList) {
+        for (T person : personList) {
             System.out.println(person);
         }
         System.out.println(personList.size());
     }
 
-public static void printInfo2 (List<Student> students){
-    for (Student student :students){
-        System.out.println(student);
+    public static void printInfo2(List<Student> students) {
+        for (Student student : students) {
+            System.out.println(student);
+        }
+        System.out.println(students.size());
     }
-    System.out.println(students.size());
-}
+
+
+    public static <T> T getLastItem(List<T> list) {
+        return list.get(list.size() - 1);
+    }
 
 
 }
