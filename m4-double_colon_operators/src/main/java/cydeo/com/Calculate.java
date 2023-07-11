d339683b7779dcaeb4ef6fc90755fa27186a07c4
ELİF DENK <elifdenkk@gmail.com>
2023-07-12 00:56:26 +0300

    Double Colon Operator

---
+++ m4-double_colon_operators/src/main/java/cydeo/com/Calculate.java
@@ -0,0 +1,5 @@
+package cydeo.com;
+@FunctionalInterface
+public interface Calculate {
+    void calculate(int x, int y);
+}
---
+++ m4-double_colon_operators/src/main/java/cydeo/com/MyClass.java
@@ -0,0 +1,7 @@
+package cydeo.com;
+
+public class MyClass {
+    public double method(int x){
+        return x*2.5;
+    }
+}
