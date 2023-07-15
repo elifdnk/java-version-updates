23e5e9118293482c7c1a1252644f3911762dfe9e
ELİF DENK <elifdenkk@gmail.com>
2023-07-15 13:06:50 +0300

    Comparator Interface

--- m5-Comparative_Interface/src/main/java/cydeo/com/AppleTest.java
+++ m5-Comparative_Interface/src/main/java/cydeo/com/AppleTest.java
@@ -1,7 +1,10 @@
 package cydeo.com;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class AppleTest {
     public static void main(String[] args) {
@@ -11,6 +14,15 @@ public class AppleTest {
         inventory.add(new Apple(100, Color.RED));
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
+
+        Comparator<Apple> sortApple = Comparator.comparing((Apple apple)-> apple.getWeight());
+        inventory.sort(sortApple) ;
+        System.out.println(inventory);
+
+       inventory.sort (comparing(Apple::getWeight));
+        System.out.println(inventory);
+
+
 
 
 
