39d11afe06762e41d9f1915efad41f4023b00fea
ELİF DENK <elifdenkk@gmail.com>
2023-07-22 14:33:52 +0300

    Finding Matching

---
+++ m6-stream/src/main/java/com/cydeo/FindingMatching.java
@@ -0,0 +1,46 @@
+package com.cydeo;
+
+import com.cydeo.task.Dish;
+import com.cydeo.task.DishData;
+
+import java.util.Optional;
+
+public class FindingMatching {
+    public static void main(String[] args) {
+
+        //ALL MATCH
+        System.out.println("ALL MATCH");
+
+        boolean isHealthy = DishData.getAll().stream()
+                .allMatch(dish -> dish.getCalories() < 1000);
+        System.out.println(isHealthy);
+        System.out.println("--------------------------------------------------");
+
+
+        //ANY MATCH
+        System.out.println("ANY MATCH");
+        if (DishData.getAll().stream().anyMatch(Dish::isVegetarian)) {
+            System.out.println("The menu is vegeterian friendly");
+        }
+        System.out.println("--------------------------------------------------");
+
+
+        //NONE MATCH
+        System.out.println("NONE MATCH");
+        boolean isHealthy2 = DishData.getAll().stream().noneMatch(dish -> dish.getCalories() >= 1000);
+        System.out.println(isHealthy2);
+        System.out.println("--------------------------------------------------");
+
+        //FIND ANY
+        System.out.println("FIND ANY");
+        Optional<Dish> dish = DishData.getAll().stream().filter(Dish::isVegetarian).findAny();
+        System.out.println(dish.get());
+        System.out.println("--------------------------------------------------");
+
+
+
+
+    }
+
+
+}
