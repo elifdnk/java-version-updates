4fef3be80ddb40afb31e442deb6673675c7e28c7
ELİF DENK <elifdenkk@gmail.com>
2023-07-22 12:15:15 +0300

    Stream

---
+++ m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
@@ -0,0 +1,28 @@
+package com.cydeo.task;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+        //print all email
+        System.out.println("print e mail");
+        EmployeeData.readAll().map(Employee::getEmpEmail)
+                .forEach(System.out::println);
+
+        //print all phone numbers
+        System.out.println("print all phone numbers");
+        EmployeeData.readAll().flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+        System.out.println("print all phone numbers with double colon");
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+
+
+    }
+}
