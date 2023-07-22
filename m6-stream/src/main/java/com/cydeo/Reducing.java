171adc48d1a687c03cf2393c96e94505f4aac637
ELİF DENK <elifdenkk@gmail.com>
2023-07-22 15:33:29 +0300

    Reducing

--- m6-stream/src/main/java/com/cydeo/Reducing.java
+++ m6-stream/src/main/java/com/cydeo/Reducing.java
@@ -1,7 +1,11 @@
 package com.cydeo;
 
+import com.cydeo.task.Dish;
+import com.cydeo.task.DishData;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Reducing {
     public static void main(String[] args) {
@@ -15,6 +19,23 @@ public class Reducing {
 
         int result = numbers.stream().reduce(0, (a, b) -> (a + b));
         System.out.println(result);
+
+
+        System.out.println("Dish calories total: ");
+        int totalCaloriesDishes = DishData.getAll().stream()
+                .map(Dish::getCalories)
+                .reduce(Integer::sum)
+                .get();
+        System.out.println(totalCaloriesDishes);
+
+        //Max and Min
+        Optional<Integer> min = numbers.stream().reduce(Integer::min);
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+
+        System.out.println("min = " + min.get());
+        System.out.println("max = " + max.get());
+        System.out.println("sum = " + sum.get());
 
 
     }
