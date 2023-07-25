306fab4c31e016bf1f3a5587993b95b5abbfce00
ELİF DENK <elifdenkk@gmail.com>
2023-07-25 18:07:39 +0300

    Records

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,15 @@
+package com.cydeo.java14;
+
+public record User(String name,String address) {
+
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING             //ALL FIELDS PRIVATE FIELD
+    //EQUALS
+    //HASH CODE
+
+    // !!!SETTER DOES NOT EXIST
+
+
+
+}
